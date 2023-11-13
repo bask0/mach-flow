af5113430e333a47640ca3041af3ca523f5ae375
bask0 <29065166+bask0@users.noreply.github.com>
2023-11-13 14:50:40 +0100

    update environment definition

--- create_env.bash
+++ create_env.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# RUN FROM PROJECT DIRECTORY! 
+
 #####################################################
-# Environment setup for conda environment 'pytorch' #
+# Environment setup for conda environment $ENVNAME  #
 #####################################################
+
+ENVNAME="machflow"
 
 # Go to base conda environment
 eval "$(conda shell.bash hook)"
@@ -11,11 +15,11 @@ mamba activate base \
     || { echo '>>> Activating base failed.'; exit 1; }
 
 # Remove environment if exists
-mamba remove --yes --name pytorch --all \
+mamba remove --yes --name $ENVNAME --all \
     || { echo '>>> Removing environment failed.'; exit 1; }
 
 # Create environment
-mamba create --yes --name pytorch python=3.10 \
+mamba create --yes --name $ENVNAME python=3.10 \
     pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 pytorch-lightning \
     numpy scikit-learn \
     pandas xarray dask netcdf4 zarr \
@@ -25,9 +29,12 @@ mamba create --yes --name pytorch python=3.10 \
     || { echo '>>> Creating environment failed.'; exit 1; }
 
 # Activate environment
-mamba activate pytorch \
+mamba activate $ENVNAME \
     || { echo '>>> Activating environment failed.'; exit 1; }
 
 # Install pip packages
 pip install torch_geometric dask-labextension pyreadr \
     || { echo '>>> Installing pip packages failed.'; exit 1; }
+
+# Add mach-flow in editable mode
+pip install -e .
