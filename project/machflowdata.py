2de4c014f2c1374ef1b7dd4ee063be0ec163a129
bask0 <29065166+bask0@users.noreply.github.com>
2023-11-20 09:56:42 +0100

    add cv_set to ds

--- project/machflowdata.py
+++ project/machflowdata.py
@@ -108,7 +108,7 @@ class MachFlowData(Dataset):
                 start_date=self.ds.time[window_start].dt.strftime('%Y-%m-%d').item(),
                 end_date=self.ds.time[window_end].dt.strftime('%Y-%m-%d').item(),
                 warmup_size=self.warmup_size,
-                cv_set=self.ds.cv_set.isel(station=station).item()
+                cv_set=self.ds.station.isel(station=station).item(),
             )
         )
 
@@ -281,7 +281,7 @@ class MachFlowDataModule(pl.LightningDataModule):
         self.train_basins, self.val_basins, self.test_basins = self.split_basins(basins=stations)
         self.predict_basins = self.ds.station.values
 
-        self.add_cv_set_id()
+        self.add_cv_set_ids()
 
         train_data = self.get_dataset('train').ds
         self.norm_args_features = Normalize.get_normalize_args(
@@ -402,13 +402,13 @@ class MachFlowDataModule(pl.LightningDataModule):
         """  
         return self.common_dataloader('predict')
 
-    def add_cv_set_id(self) -> None:
+    def add_cv_set_ids(self) -> None:
         cv_set = xr.full_like(self.ds.station, -1, dtype=np.int8)
         cv_set = cv_set.where(~cv_set.station.isin(self.train_basins), 0)
         cv_set = cv_set.where(~cv_set.station.isin(self.val_basins), 1)
         cv_set = cv_set.where(~cv_set.station.isin(self.test_basins), 2)
         cv_set.attrs['note'] = '-1=not used, 0=training, 1=validation, 2=test'
-        self.ds['cv_set'] = cv_set
+        self.ds['cv_set'] = cv_set.compute()
 
     def split_basins(
             self,
