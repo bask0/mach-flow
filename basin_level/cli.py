d24d064bc660e9c9f33cff497d485de5f143dc8e
bask0 <29065166+bask0@users.noreply.github.com>
2023-11-23 16:26:25 +0100

    change training config

--- basin_level/cli.py
+++ basin_level/cli.py
@@ -12,34 +12,45 @@ class LSTMSearchSpace(SearchSpace):
     def __init__(self, trial: optuna.Trial) -> None:
         config = {
             'model': dict(
-                num_hidden=trial.suggest_categorical('num_hidden', [16, 32, 64]),
-                num_enc=trial.suggest_categorical('num_enc', [4, 8, 16]),
+                num_hidden=trial.suggest_categorical('num_hidden', [32, 64, 128]),
+                num_enc=trial.suggest_categorical('num_enc', [4, 8, 16, 32]),
                 enc_dropout=trial.suggest_categorical('enc_dropout', [0.0, 0.2]),
-                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2]),
+                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2, 3]),
                 num_lstm_layers=trial.suggest_categorical('num_lstm_layers', [1, 2]),
             ),
             'optimizer': dict(
-                lr=trial.suggest_categorical('lr', [1e-4, 1e-3, 1e-2]),
-                weight_decay=trial.suggest_categorical('weight_decay', [0, 1e-4, 1e-2]),
+                lr=trial.suggest_categorical('lr', [1e-3, 1e-2, 1e-1]),
+                weight_decay=trial.suggest_categorical('weight_decay', [1e-1, 1e-2, 1e-3]),
             )
         }
         super().__init__(trial=trial, config=config)
 
 
-
 if __name__ == '__main__':
 
-    pruner = optuna.pruners.MedianPruner(
-        n_startup_trials=5,
-        n_warmup_steps=5)
+    # pruner = optuna.pruners.MedianPruner(
+    #     n_startup_trials=5,
+    #     n_warmup_steps=5
+    # )
 
-    sampler = optuna.samplers.RandomSampler()
+    # sampler = optuna.samplers.RandomSampler()
+
+    pruner = optuna.pruners.HyperbandPruner(
+        min_resource=10,
+        reduction_factor=2
+    )
+    sampler = optuna.samplers.TPESampler(
+        consider_prior=False,
+        n_startup_trials=10,
+        seed=1,
+        multivariate=True
+    )
 
     tuner = Tuner(
         sampler=sampler,
         pruner=pruner,
         )
 
-    tuner.tune(n_trials=50)
+    tuner.tune(n_trials=80)
 
     tuner.xval()
