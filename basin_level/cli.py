5e574b007f4b81e2db30af759f4b73e992d7a095
bask0 <29065166+bask0@users.noreply.github.com>
2023-12-12 15:38:55 +0100

    add TCN search space, change n_trials to 80

--- basin_level/cli.py
+++ basin_level/cli.py
@@ -1,6 +1,7 @@
 import optuna
 
 from models.lstm import LSTM
+from models.tcn import TCN
 from utils.tuning import SearchSpace, Tuner
 
 
@@ -12,14 +13,39 @@ class LSTMSearchSpace(SearchSpace):
     def __init__(self, trial: optuna.Trial) -> None:
         config = {
             'model': dict(
-                num_hidden=trial.suggest_categorical('num_hidden', [32, 64, 128]),
-                num_enc=trial.suggest_categorical('num_enc', [4, 8, 16, 32]),
+                num_hidden=trial.suggest_categorical('num_hidden', [64, 128, 256]),
+                num_enc=trial.suggest_categorical('num_enc', [8, 16, 32]),
                 enc_dropout=trial.suggest_categorical('enc_dropout', [0.0, 0.2]),
-                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2, 3]),
+                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2]),
                 num_lstm_layers=trial.suggest_categorical('num_lstm_layers', [1, 2]),
             ),
             'optimizer': dict(
-                lr=trial.suggest_categorical('lr', [1e-3, 1e-2, 1e-1]),
+                lr=trial.suggest_categorical('lr', [1e-4, 1e-3, 1e-2]),
+                weight_decay=trial.suggest_categorical('weight_decay', [1e-1, 1e-2, 1e-3]),
+            )
+        }
+        super().__init__(trial=trial, config=config)
+
+
+class TCNSearchSpace(SearchSpace):
+    """Defines the search space for the TCN model."""
+
+    MODEL = TCN
+
+    def __init__(self, trial: optuna.Trial) -> None:
+        config = {
+            'model': dict(
+                num_enc=trial.suggest_categorical('num_enc', [8, 16, 32]),
+                enc_dropout=trial.suggest_categorical('enc_dropout', [0.0, 0.2]),
+                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2]),
+                kernel_size=trial.suggest_categorical('kernel_size', [8, 16]),
+                num_hidden=trial.suggest_categorical('num_hidden', [64, 128, 256]),
+                num_tcn_layers=trial.suggest_categorical('num_tcn_layers', [2, 3, 4]),
+                tcn_dropout=trial.suggest_categorical('tcn_dropout', [0.0, 0.2]),
+                criterion='L1',
+            ),
+            'optimizer': dict(
+                lr=trial.suggest_categorical('lr', [1e-4, 1e-3, 1e-2]),
                 weight_decay=trial.suggest_categorical('weight_decay', [1e-1, 1e-2, 1e-3]),
             )
         }
@@ -44,6 +70,6 @@ if __name__ == '__main__':
         pruner=pruner,
         )
 
-    tuner.tune(n_trials=50)
+    tuner.tune(n_trials=80)
 
     tuner.xval()
