b91c1ab1f9e7c8e2f607382eaf38226dfd3e8634
bask0 <29065166+bask0@users.noreply.github.com>
2023-12-21 15:01:10 +0100

    use common optimizer and model config

--- basin_level/cli.py
+++ basin_level/cli.py
@@ -1,52 +1,82 @@
 import optuna
 
-from models.lstm import LSTM
-from models.tcn import TCN
+from models.base import TCN, LSTM, MHA
 from utils.tuning import SearchSpace, Tuner
 
 
+def get_enc_search_space(trial: optuna.Trial) -> dict:
+
+    config = dict(
+        model_dim=trial.suggest_categorical('model_dim', [64, 128, 256]),
+        enc_dropout=trial.suggest_categorical('enc_dropout', [0.0, 0.2]),
+        enc_layers=trial.suggest_categorical('enc_layers', [1, 2]),
+    )
+
+    return config
+
+def get_optimizer_search_space(trial: optuna.Trial) -> dict:
+
+    config = dict(
+        lr=trial.suggest_categorical('lr', [1e-4, 1e-3, 1e-2]),
+        weight_decay=trial.suggest_categorical('weight_decay', [1e-1, 1e-2, 1e-3]),
+    )
+
+    return config
+
+
 class LSTMSearchSpace(SearchSpace):
-    """Defines the search space for the LSTM model."""
+    """Defines the search space for the lonh short-term memory (LSTM) model."""
 
     MODEL = LSTM
 
     def __init__(self, trial: optuna.Trial) -> None:
         config = {
             'model': dict(
-                num_hidden=trial.suggest_categorical('num_hidden', [64, 128, 256]),
-                num_enc=trial.suggest_categorical('num_enc', [8, 16, 32]),
-                enc_dropout=trial.suggest_categorical('enc_dropout', [0.0, 0.2]),
-                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2]),
-                num_lstm_layers=trial.suggest_categorical('num_lstm_layers', [1, 2]),
+                **get_enc_search_space(trial),
+                lstm_layers=trial.suggest_categorical('lstm_layers', [1, 2]),
             ),
             'optimizer': dict(
-                lr=trial.suggest_categorical('lr', [1e-4, 1e-3, 1e-2]),
-                weight_decay=trial.suggest_categorical('weight_decay', [1e-1, 1e-2, 1e-3]),
+                **get_optimizer_search_space(trial),
             )
         }
         super().__init__(trial=trial, config=config)
 
 
 class TCNSearchSpace(SearchSpace):
-    """Defines the search space for the TCN model."""
+    """Defines the search space for the temporal convolutional network (TCN) model."""
 
     MODEL = TCN
 
     def __init__(self, trial: optuna.Trial) -> None:
         config = {
             'model': dict(
-                num_enc=trial.suggest_categorical('num_enc', [8, 16, 32]),
-                enc_dropout=trial.suggest_categorical('enc_dropout', [0.0, 0.2]),
-                num_enc_layers=trial.suggest_categorical('num_enc_layers', [1, 2]),
-                kernel_size=trial.suggest_categorical('kernel_size', [8, 16]),
-                num_hidden=trial.suggest_categorical('num_hidden', [64, 128, 256]),
-                num_tcn_layers=trial.suggest_categorical('num_tcn_layers', [2, 3, 4]),
+                **get_enc_search_space(trial),
+                tcn_kernel_size=trial.suggest_categorical('tcn_kernel_size', [8, 16]),
+                tcn_layers=trial.suggest_categorical('tcn_layers', [2, 3, 4]),
                 tcn_dropout=trial.suggest_categorical('tcn_dropout', [0.0, 0.2]),
-                criterion='L1',
             ),
             'optimizer': dict(
-                lr=trial.suggest_categorical('lr', [1e-4, 1e-3, 1e-2]),
-                weight_decay=trial.suggest_categorical('weight_decay', [1e-1, 1e-2, 1e-3]),
+                **get_optimizer_search_space(trial),
+            )
+        }
+        super().__init__(trial=trial, config=config)
+
+
+class MHASearchSpace(SearchSpace):
+    """Defines the search space for the multihead attention (MHA) model."""
+
+    MODEL = MHA
+
+    def __init__(self, trial: optuna.Trial) -> None:
+        config = {
+            'model': dict(
+                **get_enc_search_space(trial),
+                mha_nhead=trial.suggest_categorical('mha_nhead', [2, 4, 8]),
+                mha_layers=trial.suggest_categorical('mha_layers', [1, 2, 3]),
+                mha_dropout=trial.suggest_categorical('mha_dropout', [0.0, 0.1, 0.2]),
+            ),
+            'optimizer': dict(
+                **get_optimizer_search_space(trial),
             )
         }
         super().__init__(trial=trial, config=config)
@@ -70,6 +100,6 @@ if __name__ == '__main__':
         pruner=pruner,
         )
 
-    tuner.tune(n_trials=80)
+    tuner.tune(n_trials=20)
 
     tuner.xval()
