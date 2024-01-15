58b2e3c4eeca5da8497e993591d6fef4690d7f10
bask0 <29065166+bask0@users.noreply.github.com>
2024-01-15 13:18:22 +0000

    add more fusion_method options

--- basin_level/run_experiment.sh
+++ basin_level/run_experiment.sh
@@ -36,6 +36,7 @@ SCRIPT_PATH="basin_level/cli.py"
 COMMAND="python ${SCRIPT_PATH} --model ${model}"
 
 for c in {""," -c basin_level/staticall.yaml"," -c basin_level/staticdred.yaml"}\
+`       `{""," -c basin_level/allbasins.yaml"}\
 `       `{""," -c basin_level/sqrttrans.yaml"} ; do
     CMD="${COMMAND}${c}"
     echo "Running: ${CMD}"
