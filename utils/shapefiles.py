6315a52558fdb5e32ece5a01411e0724d86679cb
bask0 <29065166+bask0@users.noreply.github.com>
2024-02-22 09:58:35 +0000

    also return gauging stations

--- utils/shapefiles.py
+++ utils/shapefiles.py
@@ -5,29 +5,41 @@ from pathlib import Path
 import os
 
 
-def get_shapefile(source: str, datapath: str | os.PathLike = '/mydata/machflow/basil/data/') -> gpd.GeoDataFrame:
+def get_shapefile(
+        source: str,
+        datapath: str | os.PathLike = '/mydata/machflow/basil/data/'
+        ) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
 
     datapath = Path(datapath)
 
     if source == 'obs':
-        obs = gpd.read_file('/mydata/machflow/basil/data/obs/shapefile/shape_tabsd.shp')
+        obs = gpd.read_file(datapath / 'obs/shapefile/shape_tabsd.shp')
+        obs_point = gpd.read_file(datapath / 'obs/shapefile/stations_tabsd.shp')
         id_field = 'mach_ID'
     elif source == 'prevah':
-        obs = gpd.read_file('/mydata/machflow/basil/data/prevah/shapefile/Regions_307.shp')
+        obs = gpd.read_file(datapath / 'prevah/shapefile/Regions_307.shp')
+        obs_point = obs
         id_field = 'OBJECTID'
         obs[id_field] = [f'HSU_{int(id):03d}' for id in obs[id_field].astype(str)]
     else:
         raise ValueError('`source` must be on fo \'obs\', \'prevah\', is \'{source}\'.')
 
-    ds = xr.open_zarr('/mydata/machflow/basil/data/harmonized_basins.zarr/')
+    obs = obs.rename(columns={id_field: 'OID'}).set_index('OID')
+    obs_point = obs_point.rename(columns={id_field: 'OID'}).set_index('OID')
+    obs_point = obs_point.reindex_like(obs)
+    obs = obs.reset_index()
+    obs_point = obs_point.reset_index()
 
-    common_ids = [oid for oid in obs[id_field].values if oid in ds.station.values]
-    obs = obs[obs[id_field].isin(common_ids)]
+    ds = xr.open_zarr(datapath / 'harmonized_basins.zarr/')
+    common_ids = [oid for oid in obs['OID'].values if oid in ds.station.values]
+
+    obs = obs[obs['OID'].isin(common_ids)]
+    obs_point = obs_point[obs_point['OID'].isin(common_ids)]
     ds = ds.sel(station=common_ids)
-    obs['is_train'] = (ds.folds >= 0).sel(station=common_ids)
 
-    obs = obs.rename(columns={id_field: 'OID'})
+    obs['is_train'] = (ds.folds >= 0).sel(station=common_ids)
+    obs_point['is_train'] = (ds.folds >= 0).sel(station=common_ids)
 
     obs['Shape_Area'] = obs['Shape_Area'] / 1000000
 
-    return obs
+    return obs, obs_point
