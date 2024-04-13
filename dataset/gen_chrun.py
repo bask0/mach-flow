a8a05bbd55d16d5944beb329afdf5affb2d62f63
bask0 <29065166+bask0@users.noreply.github.com>
2024-04-13 15:02:44 +0000

    update paths and data harmonization

--- dataset/gen_chrun.py
+++ dataset/gen_chrun.py
@@ -1,16 +1,21 @@
 import geopandas as gpd
 import os
+from pathlib import Path
 
 from utils.data import load_xval
 
-BEST_MODEL_PATH = '/mydata/machflow/basil/runs/basin_level/staticall_allbasins_sqrttrans/LSTM/'
-BEST_MODEL_XVAL_PATH = BEST_MODEL_PATH + 'xval/'
-OUT_PATH = BEST_MODEL_PATH + 'chrun/'
+# BEST_MODEL_PATH = '/mydata/machflow/basil/runs/basin_level/staticall_allbasins_sqrttrans/LSTM/'
+SHAPEFILE_PATH = Path('/mydata/machflow/shared/data/RawFromMichael/prevah_307/shapefile/')
+BEST_MODEL_PATH = Path('/mydata/machflow/basil/runs/basin_level/staticall_allbasins_sqrttrans/LSTM/')
+BEST_MODEL_XVAL_PATH = BEST_MODEL_PATH / 'xval/'
+OUT_PATH = Path('/mydata/machflow/basil/runs/basin_level/CHRUN/')
+
+CHRUN_OUT_PATH = OUT_PATH / 'chrun.nc'
+CATCHMENTS_OUT_PATH = OUT_PATH / 'catchments/'
+README_OUT_PATH = OUT_PATH / 'readme.txt'
 
 if not os.path.exists(OUT_PATH):
     os.makedirs(OUT_PATH)
-
-exit(0)
 
 static_names = dict(
     water=1,
@@ -41,6 +46,7 @@ static_names = dict(
 ds = load_xval(BEST_MODEL_XVAL_PATH, sources=0).median('cv').isel(tau=0).drop([
     'tau',
     'E',
+    'Qmm',
     'Qm3s',
     'Qm3s_prevah',
     'c0',
@@ -51,14 +57,15 @@ ds = load_xval(BEST_MODEL_XVAL_PATH, sources=0).median('cv').isel(tau=0).drop([
     'cv_set',
     'folds'
 ]
-).load()
+).sel(time=slice('1962', '2023'))
 
+ds = ds.load()
 
 global_attrs = dict(
     title='CH-RUN: A data-driven spatially contiguous runoff monitoring product for Switzerland',
     institution='IAC, ETH Zurich',
     source='CH-RUN',
-    comment='Observed runoff: Qmm, modeled runoff product (CH-RUN): Qmm_mod, PREVAH runoff: Qmm_prevah.',
+    comment='Modeled runoff product (CH-RUN): Qmm_mod, PREVAH runoff: Qmm_prevah.',
     references='INSERT HESS PAPER',
     version_number='1.0',
     contact='Basil Kraft (basilkraft@env.ethz.ch), Lukas Gudmundsson (lukas.gudmundsson@env.ethz.ch)'
@@ -82,14 +89,7 @@ variable_attrs = {
         source='MeteoSwiss',
         references='MeteoSwiss: Daily Mean, Minimum and Maximum Temperature: TabsD, TminD, TmaxD, https://www.meteoschweiz.admin.ch/dam/jcr:818a4d17-cb0c-4e8b-92c6-1a1bdf5348b7/ProdDoc_TabsD.pdf, last access: 1 April 2024.',
         long_name='Air temperature',
-        units='°C'
-    ),
-    'Qmm': dict(
-        title='Observed runoff',
-        source='1) CAMELS-CH, 2) FOEN',
-        references='1) Höge, M., Scheidegger, A., Baity-Jesi, M., Albert, C., and Fenicia, F.: Improving Hydrologic Models for Predictions and Process Understanding Using Neural ODEs, Hydrology and Earth System Sciences, 26, 5085–5102, https://doi.org/10.5194/hess-26-5085-2022, 2022. 2) FOEN: Hydrological Data Service for Watercourses and Lakes, https://www.bafu.admin.ch/bafu/en/home/themen/thema-wasser/wasser–daten–indikatoren-und-karten/wasser–messwerte-und-statistik/messwerte-zum-thema-wasser-beziehen/datenservice-hydrologie-fuer-fliessgewaesser-und-seen.html, last access: 1 April 2024, 2024.',
-        long_name='Runoff',
-        units='mm d-1'
+        units='degC'
     ),
     'Qmm_mod': dict(
         title='CH-RUN runoff',
@@ -101,7 +101,7 @@ variable_attrs = {
     'Qmm_prevah': dict(
         title='PREVAH runoff',
         source='WSL',
-        references='1) Viviroli, D., Zappa, M., Schwanbeck, J., Gurtz, J., and Weingartner, R.: Continuous Simulation for Flood Estimation in Ungauged Mesoscale Catchments of Switzerland – Part I: Modelling Framework and Calibration Results, Journal of Hydrology, 377, 191–207, https://doi.org/10.1016/j.jhydrol.2009.08.023, 2009c. 2) Viviroli, D., Zappa, M., Gurtz, J., and Weingartner, R.: An Introduction to the Hydrological Modelling System PREVAH and Its Pre- and Post-Processing-Tools, Environmental Modelling & Software, 24, 1209–1222, https://doi.org/10.1016/j.envsoft.2009.04.001, 2009b.',
+        references='1) Viviroli, D., Zappa, M., Schwanbeck, J., Gurtz, J., and Weingartner, R.: Continuous Simulation for Flood Estimation in Ungauged Mesoscale Catchments of Switzerland – Part I: Modelling Framework and Calibration Results, Journal of Hydrology, 377, 191–207, https://doi.org/10.1016/j.jhydrol.2009.08.023, 2009. 2) Viviroli, D., Zappa, M., Gurtz, J., and Weingartner, R.: An Introduction to the Hydrological Modelling System PREVAH and Its Pre- and Post-Processing-Tools, Environmental Modelling & Software, 24, 1209–1222, https://doi.org/10.1016/j.envsoft.2009.04.001, 2009.',
         long_name='Runoff (PREVAH)',
         units='mm d-1'
     ),
@@ -136,28 +136,28 @@ variable_attrs = {
     'btk': dict(
         title='Soil depth',
         source='1) Soilgrids, 2) Hochaufloesende Bodenkarten für den Schweizer Wald',
-        references='1) Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: Producing Soil Information for the Globe with Quantified Spatial Uncertainty, SOIL, 7, 217–240, https://doi.org/10.5194/soil-7-217-2021, 2021. 2) Baltensweiler, A., Walthert, L., Zimmermann, S., and Nussbaum, M.: Hochauflösende Bodenkarten Für Den Schweizer Wald, Schweizerische Zeitschrift fur Forstwesen, 173, 288–291, https://doi.org/10.3188/szf.2022.0288, 2022.',
+        references='1) Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: Producing Soil Information for the Globe with Quantified Spatial Uncertainty, SOIL, 7, 217–240, https://doi.org/10.5194/soil-7-217-2021, 2021. 2) Baltensweiler, A., Walthert, L., Zimmermann, S., and Nussbaum, M.: Hochaufloesende Bodenkarten Für Den Schweizer Wald, Schweizerische Zeitschrift fur Forstwesen, 173, 288–291, https://doi.org/10.3188/szf.2022.0288, 2022.',
         long_name='Soil depth',
         units='m'
     ),
     'kwt': dict(
         title='Hydraulic conductivity',
         source='1) Soilgrids, 2) Hochaufloesende Bodenkarten für den Schweizer Wald',
-        references='1) Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: Producing Soil Information for the Globe with Quantified Spatial Uncertainty, SOIL, 7, 217–240, https://doi.org/10.5194/soil-7-217-2021, 2021. 2) Baltensweiler, A., Walthert, L., Zimmermann, S., and Nussbaum, M.: Hochauflösende Bodenkarten Für Den Schweizer Wald, Schweizerische Zeitschrift fur Forstwesen, 173, 288–291, https://doi.org/10.3188/szf.2022.0288, 2022.',
+        references='1) Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: Producing Soil Information for the Globe with Quantified Spatial Uncertainty, SOIL, 7, 217–240, https://doi.org/10.5194/soil-7-217-2021, 2021. 2) Baltensweiler, A., Walthert, L., Zimmermann, S., and Nussbaum, M.: Hochaufloesende Bodenkarten Für Den Schweizer Wald, Schweizerische Zeitschrift fur Forstwesen, 173, 288–291, https://doi.org/10.3188/szf.2022.0288, 2022.',
         long_name='Hydraulic conductivity',
         units='mm h-1'
     ),
     'pfc': dict(
         title='Soil water storage capacity',
         source='1) Soilgrids, 2) Hochaufloesende Bodenkarten für den Schweizer Wald',
-        references='1) Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: Producing Soil Information for the Globe with Quantified Spatial Uncertainty, SOIL, 7, 217–240, https://doi.org/10.5194/soil-7-217-2021, 2021. 2) Baltensweiler, A., Walthert, L., Zimmermann, S., and Nussbaum, M.: Hochauflösende Bodenkarten Für Den Schweizer Wald, Schweizerische Zeitschrift fur Forstwesen, 173, 288–291, https://doi.org/10.3188/szf.2022.0288, 2022.',
+        references='1) Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: Producing Soil Information for the Globe with Quantified Spatial Uncertainty, SOIL, 7, 217–240, https://doi.org/10.5194/soil-7-217-2021, 2021. 2) Baltensweiler, A., Walthert, L., Zimmermann, S., and Nussbaum, M.: Hochaufloesende Bodenkarten Für Den Schweizer Wald, Schweizerische Zeitschrift fur Forstwesen, 173, 288–291, https://doi.org/10.3188/szf.2022.0288, 2022.',
         long_name='Soil water storage capacity',
         units='%'
     ),
     'glm': dict(
         title='Glacier morphology',
         source='SGI2016',
-        references='Linsbauer, A., Huss, M., Hodel, E., Bauder, A., Fischer, M., Weidmann, Y., Bärtschi, H., and Schmassmann, E.: The New Swiss Glacier Inventory SGI2016: From a Topographical to a Glaciological Dataset, Frontiers in Earth Science, 9, https://doi.org/10.3389/feart.2021.704189, 2021.',
+        references='Linsbauer, A., Huss, M., Hodel, E., Bauder, A., Fischer, M., Weidmann, Y., Baertschi, H., and Schmassmann, E.: The New Swiss Glacier Inventory SGI2016: From a Topographical to a Glaciological Dataset, Frontiers in Earth Science, 9, https://doi.org/10.3389/feart.2021.704189, 2021.',
         long_name='Glacier morphology',
         units='-'
     ),
@@ -173,7 +173,8 @@ variable_attrs = {
 
 ds.attrs = global_attrs
 for var, attrs in variable_attrs.items():
-    attrs = {**default_attrs, **attrs}
+    if not var.startswith('Qmm'):
+        attrs = {**default_attrs, **attrs}
     ds[var].attrs = attrs
 
 for k, v in static_names.items():
@@ -182,8 +183,8 @@ for k, v in static_names.items():
 
     attrs = dict(
         title=name_nice,
-        source='WSL',
-        references='1) Viviroli, D., Zappa, M., Schwanbeck, J., Gurtz, J., and Weingartner, R.: Continuous Simulation for Flood Estimation in Ungauged Mesoscale Catchments of Switzerland – Part I: Modelling Framework and Calibration Results, Journal of Hydrology, 377, 191–207, https://doi.org/10.1016/j.jhydrol.2009.08.023, 2009c. 2) Viviroli, D., Zappa, M., Gurtz, J., and Weingartner, R.: An Introduction to the Hydrological Modelling System PREVAH and Its Pre- and Post-Processing-Tools, Environmental Modelling & Software, 24, 1209–1222, https://doi.org/10.1016/j.envsoft.2009.04.001, 2009b.',
+        source='1) Habitat Map of Switzerland 2) WSL',
+        references='1) Price, B., Huber, N., Nussbaumer, A., and Ginzler, C.: The Habitat Map of Switzerland: A Remote Sensing, Composite Approach for a High Spatial and Thematic Resolution Product, Remote Sensing, 15, 643, https://doi.org/10.3390/rs15030643, 2023. 2) Viviroli, D., Zappa, M., Schwanbeck, J., Gurtz, J., and Weingartner, R.: Continuous Simulation for Flood Estimation in Ungauged Mesoscale Catchments of Switzerland – Part I: Modelling Framework and Calibration Results, Journal of Hydrology, 377, 191–207, https://doi.org/10.1016/j.jhydrol.2009.08.023, 2009.',
         long_name=name_nice,
         units='-'
     )
@@ -193,8 +194,31 @@ for k, v in static_names.items():
         ds = ds.rename({pus_name: pus_name_frac})
         ds[pus_name_frac].attrs = attrs
 
-ds.to_netcdf(OUT_PATH + '/chrun.nc')
 
-df = gpd.read_file('/mydata/machflow/shared/data/RawFromMichael/prevah_307/shapefile/')
+df = gpd.read_file(SHAPEFILE_PATH)
 df['station'] = [f'HSU_{obj:03d}' for obj in df['OBJECTID']]
 df = df.drop(columns=['Shape_Leng', 'Shape_Area', 'OBJECTID']).set_index('station')
+
+README = """
+CH-RUN V1 --- Kraft et al. (2024)
+
+Datasets
+-----------
+- catchments: ESRI shapefiles containing the 307 catchments.
+- chrun: NetCDF file containing model features and simulated runoff rates by CH-RUN and PREVAH.
+
+Usage
+-----------
+The catchments provided as shapefile can be linked with the NetCDF data via the 'station' field.
+
+References
+-----------
+Add HESS  reference.
+
+"""
+
+df.to_file(CATCHMENTS_OUT_PATH)
+ds.to_netcdf(CHRUN_OUT_PATH)
+with open(README_OUT_PATH, 'w') as f:
+    f.write(README)
+
