a4dc1d94af94a6bc072c3bde85fd480e762419b4
bask0 <29065166+bask0@users.noreply.github.com>
2024-10-21 12:21:25 +0200

    create file

--- analysis/config.py
+++ analysis/config.py
@@ -4,6 +4,7 @@ from pathlib import Path
 def get_path_dict() -> dict[str, Path]:
     path_dict = dict(
         figures=Path('/net/argon/landclim/kraftb/machflow/mach-flow/analysis/figures/'),
+        figures_alt=Path('/net/argon/landclim/kraftb/machflow/mach-flow/analysis/helper_figures/'),
         runs=Path('/net/argon/landclim/kraftb/machflow/runs/'),
         data=Path('/net/argon/landclim/kraftb/machflow/data/'),
     )
