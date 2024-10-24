f33bc4ad0a3a25450f5340eabce932088a7e0cd5
bask0 <29065166+bask0@users.noreply.github.com>
2024-10-24 11:52:09 +0200

    change plot order

--- create_plots.sh
+++ create_plots.sh
@@ -7,11 +7,11 @@ python analysis/catchment_vis.py
 echo ">>> Model visualization (fig03.png + figA01.png)."
 python analysis/model_vis.py
 
-echo ">>> Annual visualization (fig04.png)."
-python analysis/annual_vis.py
-
-echo ">>> Spatial visualization (fig05.png)."
+echo ">>> Spatial visualization (fig04.png)."
 python analysis/spatial_vis.py
+
+echo ">>> Annual visualization (fig05.png)."
+python analysis/annual_vis.py
 
 echo ">>> Timeseries visualization (fig06.png)."
 python analysis/timeseries_vis.py
